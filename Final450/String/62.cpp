65b8de2f8c8a5abb52abbaf7a8c63a659d74aa43
Abhay Maurya <abhaymaurya811@gmail.com>
2021-06-30 06:23:57 +0530

    count and say

--- Final450/String/62.cpp
+++ Final450/String/62.cpp
@@ -1,8 +1,32 @@
+// Count and Say Problem
+
 #include "../temp.h"
 
 int main()
 {
     int n; 
     cin >> n;
-    cout << countAndSay(n);
+    string s = "1";
+
+    if (n == 1)
+        cout << s << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        string t = "";
+        char temp = s[0];
+        int c = 1, j;
+        for (j = 1; j < s.size(); j++)
+        {
+            if (temp != s[j])
+            {
+                t = t + to_string(c) + temp;
+                temp = s[j];
+                c = 0; 
+            }
+            c++;
+        }
+        t = t + to_string(c) + temp;
+        s = t;
+    }
+    cout << s << endl;
 }
